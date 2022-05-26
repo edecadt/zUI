fed8b1f6b2bd5fd3f6e5e2395e52de62c2b44f18
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2022-05-26 09:50:00 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     window.addEventListener('message', function(event) {
         var data = event.data
+        
         $(".hud-container").css("display",data.show? "none":"flex");
         if (data.armor <= 0) {
             $(".shield-container").css("display","none");
@@ -8,9 +9,9 @@ $(document).ready(function(){
             $(".shield-container").css("display","block");
             $(".shield-bar").css("width", data.armor-3 + "%");
         }
-
         $(".health-bar").css("width", data.health/2-3 + "%");
         $(".hunger-bar").css("width", data.hunger-3 + "%");
         $(".thirst-bar").css("width", data.thirst-3 + "%");
     });
 });
+
